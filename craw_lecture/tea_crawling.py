0529782c73060d7e6ea21c5bfb5362768c8532af
danial1021 <41097199+danial1021@users.noreply.github.com>
2020-06-05 10:39:53 +0900

    Use Pandas
    Crawl food names and prices and turn them into dataframes

--- craw_lecture/tea_crawling.py
+++ craw_lecture/tea_crawling.py
@@ -2,6 +2,8 @@ from selenium import webdriver as wb
 import time
 from bs4 import BeautifulSoup as bs
 import pandas as pd
+# import numpy
+# print(numpy.__version__)
 
 driver = wb.Chrome()
 
@@ -14,6 +16,23 @@ for _ in range(3):
     btn.click()
     time.sleep(2)
 
+
 soup = bs(driver.source_page, 'html.parser')
-soup.select("")
+
+name_list = []
+name_tag_list = soup.select("h4.h.fz_03")
+for name_tag in name_tag_list:
+    name_list.append(name_tag.text)
+print(name_list)
+
+price_tag_list = soup.select("div.item-price > strong")
+price_list = []
+for price_tag in price_tag_list:
+    price_list.append(price_tag.text)
+print(price_list)
     
+dic = {'name':name_list, 'price':price_list}
+data = pd.DataFrame(dic)
+
+data.to_csv("한솥.csv", index=False, encoding='euc-kr')
+pd.read_csv("한솥.csv", encoding='euc-kr')
